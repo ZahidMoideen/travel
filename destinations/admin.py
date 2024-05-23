db0079eb4af6f00d89550dd3576fab7140af6b61
zahid <zahidmoideen7781@gmail.com>
2024-05-23 22:45:08 +0530

    up

--- destinations/admin.py
+++ destinations/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Destination
 
-# Register your models here.
+@admin.register(Destination)
+class DestinationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'country', 'best_time_to_visit', 'category', 'created_at', 'updated_at']
+    search_fields = ['name', 'country']
+    list_filter = ['category']
