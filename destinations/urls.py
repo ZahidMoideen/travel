52b8fcceed9a08b7406d2098b4d91ca7e6cd745b
zahid <zahidmoideen7781@gmail.com>
2024-05-25 01:13:21 +0530

    Authentication

--- destinations/urls.py
+++ destinations/urls.py
@@ -1,12 +1,11 @@
-
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import DestinationListCreate, DestinationRetrieveUpdateDestroy, UserCreate
+from .views import DestinationListCreate, DestinationRetrieveUpdateDestroy, UserCreate, UserLogin
 
 urlpatterns = [
     path('destinations/', DestinationListCreate.as_view(), name='destination-list-create'),
     path('destinations/<int:pk>/', DestinationRetrieveUpdateDestroy.as_view(), name='destination-detail'),
-    path('api/authtoken/',obtain_auth_token, name='api_token_auth'),
+    path('api/token/', obtain_auth_token, name='api_token_auth'),
+    path('login/', UserLogin.as_view(), name='user-login'),
     path('register/', UserCreate.as_view(), name='user-register'),
-
 ]
--- destinations/views.py
+++ destinations/views.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import User
 from .models import Destination
 from .serializers import DestinationSerializer, UserSerializer
 from .views_base import BaseAPIView
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from rest_framework.views import APIView
 
 class DestinationListCreate(BaseAPIView, generics.ListCreateAPIView):
     queryset = Destination.objects.all()
@@ -48,3 +52,12 @@ class UserCreate(BaseAPIView, generics.CreateAPIView):
             return super().create(request, *args, **kwargs)
         except Exception as e:
             return self.handle_exception(e)
+
+class UserLogin(APIView):
+    def post(self, request, *args, **kwargs):
+        obtain_auth_token = ObtainAuthToken()
+        serializer = obtain_auth_token.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key, 'user_id': user.id}, status=HTTP_200_OK)
